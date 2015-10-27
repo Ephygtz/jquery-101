61a0b68c929508cda006b8f08bd939693d7eb3a5
Ephraim <ephygtz@gmail.com>
2015-10-27 14:59:12 +0300

    Initial commit

---
+++ jsfile.js
@@ -0,0 +1,24 @@
+// Check if document has loaded..
+	$(document).ready(function() {
+		
+		$('p').hide(); //Hides element
+
+		$('h2').mouseenter(function () {
+			$("p").show();	// body...
+			$("body").css("background-color", "green");
+		})
+
+		$("#btn1").click(function () {
+			// body...
+			$("#content").append("This is my DIV with more content </br>");
+		})
+		$("#btn2").click(function () {
+			// body...
+			$(".fade").fadeOut("slow");
+		})
+
+		//Add a css class to elements Div
+		$("#btn1").addClass("leftcontent");
+		$("#remove").addClass("rightcontent");
+		$("#font").addClass("increasefont");
+})
